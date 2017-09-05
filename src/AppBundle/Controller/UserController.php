b2710bc9d2619ee3f99b5bf3c466c55a8a33993b
ezequiel <ezgatrabajo@gmail.com>
2017-09-04 23:20:53 -0300

    login

---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Producto;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @Route("producto")
+ */
+class UserController extends Controller
+{
+    
+    
+}
