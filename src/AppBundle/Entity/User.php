e7e42ae081049507752f14428aa4702b46a0dc04
egalvan <ezgatrabajo@gmail.com>
2017-09-07 17:09:32 -0300

    algo

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -42,5 +42,15 @@ class User extends BaseUser
         return $this;
     }
     
+    
+     /**
+     * @ORM\ManyToMany(targetEntity="AppBundle\Entity\Group")
+     * @ORM\JoinTable(name="fos_user_user_group",
+     *      joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id")},
+     *      inverseJoinColumns={@ORM\JoinColumn(name="group_id", referencedColumnName="id")}
+     * )
+     */
+    protected $groups;
+    
      
 }
