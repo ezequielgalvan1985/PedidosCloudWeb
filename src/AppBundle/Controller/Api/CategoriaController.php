349839f2c57625cbdb19c68d1cc3a791c3c51ccc
egalvan <ezgatrabajo@gmail.com>
2017-10-02 15:46:57 -0300

    h

--- src/AppBundle/Controller/Api/CategoriaController.php
+++ src/AppBundle/Controller/Api/CategoriaController.php
@@ -14,6 +14,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use\Symfony\Component\HttpFoundation\JsonResponse;
 
 use Symfony\Component\Serializer\Serializer;
@@ -31,6 +32,31 @@ class CategoriaController extends FOSRestController{
     public function getCategoriasAction($empresa_id){
         $empresa = $this->getDoctrine()->getRepository('AppBundle:Empresa')->findById($empresa_id);
         $result = $this->getDoctrine()->getRepository('AppBundle:Categoria')->findByEmpresa($empresa);
+        if ($result === null) {
+            $respuesta = array('code'=>'500',
+                           'message'=>'No se encontraron registros',
+                           'data'=>$result
+                        );
+        }else{
+            $respuesta = array('code'=>'200',
+                           'message'=>'ok',
+                           'data'=>$result
+                        );
+        
+        }
+        return $respuesta;
+    }
+
+    /**
+    * @Rest\Post("/api/categorias/{empresa_id}")
+    */
+    public function postCategoriasAction($empresa_id, Request $request){
+        $empresa = $this->getDoctrine()->getRepository('AppBundle:Empresa')->findById($empresa_id);
+        //Leer Categoria
+        $categoria = new Categoria();
+        $categoria->setEmpresa($empresa);
+        $categoria->setNombre($request->get('nombre'));
+        $categoria->setDescripcion($request->get('descripcion'));
         if ($result === null) {
             $respuesta = array('code'=>'500',
                            'message'=>'No se encontraron registros',
